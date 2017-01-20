764d0b2bf99960a1addd148f0d6a815fc02e85ac
VolodyaKarapuz <VolodyaKarapuz42@gmail.com>
2017-01-20 19:08:27 +0300

    Потоки

--- Third.cpp
+++ Third.cpp
@@ -14,39 +14,86 @@
 #include <fstream>
 #include <math.h>
 #include <stdlib.h>
+#include <mutex>
+#include <thread>
+#include <unistd.h> 
 
 
 
  
 using namespace std;
 
-void gets_utf8(FILE *in, FILE *out)
+
+
+
+
+void foo (mutex& mtx, FILE *in, FILE *out)
 {
+	
     char *buf,str2;
 	wchar_t tbuf[100];
 	char str[50];
 	ifstream f;
 	ofstream f1;
 	f1.open("out.txt", ios_base::app);	
-    while (!feof(in))
+	while (!feof(in))
 	{
 		buf = fgets (str,sizeof(str),in);
 		MultiByteToWideChar(1251,0,buf,-1,tbuf,100);
 		str2 = WideCharToMultiByte(CP_UTF8,0,tbuf,-1,buf,100,NULL,NULL); //4110 CP_UTF8
 		
 		f1<<str2;
+		cout << "POTOK1"<< endl;
 	}
+	f1.close();
+
+	
+}
+
+
+void bar (mutex& mtx, FILE *in2, FILE *out2)
+{
+	
+    char *buf,str2;
+	wchar_t tbuf[100];
+	char str[50];
+	ifstream f;
+	ofstream f1;
+	f1.open("out2.txt", ios_base::app);	
+	while (!feof(in2))
+	{
+		buf = fgets (str,sizeof(str),in2);
+		MultiByteToWideChar(1251,0,buf,-1,tbuf,100);
+		str2 = WideCharToMultiByte(CP_UTF8,0,tbuf,-1,buf,100,NULL,NULL); //4110 CP_UTF8
+		
+		f1<<str2;
+		cout << "POTOK2"<< endl;
+	}
+	f1.close();
+	
 }
 
 int main(int argc, char *argv[])
 {
-	FILE *in, *out;
-
+	FILE *in, *out, *in2, *out2;
+	mutex mtx;
 	in = fopen ("in.txt","r+");
-	out = fopen ("out.txt","w");	
+	in2 = fopen ("in2.txt","r+");
+	out = fopen ("out.txt","w");
+	out2 = fopen ("out.txt","w");	
 	fclose(out);
-	gets_utf8(in,out);
+	
+	
+	thread t1 (foo, ref(mtx), ref(in), ref(out));
+	thread t2 (bar, ref(mtx), ref(in2), ref(out));
+	
+	t1.join();
+	t2.join();
+
 	fclose(in);
 	fclose(out);
+	fclose(in2);
+	fclose(out2);
+	
 }
 
